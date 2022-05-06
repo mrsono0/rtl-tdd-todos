9a343dbd443087e995179c221cf38e6a05b31bf8
mrsono0 <mrsono0@google.com>
2022-05-06 10:38:47 +0900

    TodoFrom Submit event test ok

--- src/TodoFrom.js
+++ src/TodoFrom.js
@@ -1,10 +1,19 @@
-import react from "react";
+import react, { useCallback, useState } from "react";
 
-const TodoFrom = () => {
+const TodoFrom = ({onInsert}) => {
+    const [value, setValue] = useState("");
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault();
+    }, [onInsert, value]);
     return (
         <div>
-            <form>
-                <input placeholder="할 일을 입력하세요" />
+            <form onSubmit={onSubmit}>
+                <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
                 <button type="submit">등록</button>
             </form>
         </div>
--- src/TodoFrom.test.js
+++ src/TodoFrom.test.js
@@ -1,11 +1,38 @@
 import React from 'react'
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import TodoFrom from './TodoFrom';
+import { toHaveAttribute } from '@testing-library/jest-dom/dist/matchers';
 
 describe('<TodoForm />', () => {
     it('has input and a button', () => {
         const {getByPlaceholderText, getByText} = render(<TodoFrom />);
         getByPlaceholderText('할 일을 입력하세요'); //input 이 출력이 되었는지 확인 하는 조건
         getByText('등록'); // button 이 출력 되었는지 확인
+    })
+
+    it('changes input', () => {
+        const {getByPlaceholderText} = render(<TodoFrom />);
+        const input = getByPlaceholderText('할 일을 입력하세요');
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD 배우기'
+            }
+        })
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
+    })
+
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn();
+        const {getByPlaceholderText, getByText} = render(<TodoFrom onInsert={onInsert} />)
+        const input = getByPlaceholderText('할 일을 입력하세요');
+        const button = getByText('등록');
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD 배우기'
+            }
+        });
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기');
+        expect(input).toHaveAttribute('value', '');
     })
 })
